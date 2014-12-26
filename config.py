6fbb64bdee6feb53146042dafb72762f8dd3ca18
Johannes Neumeier <hello@johannesneumeier.com>
2014-12-26 17:48:56 +0200

    for now there doesn't seem a better way to keep config out of the repo but active for heroku, so it's back in :/

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-# copy this as config.py and insert correct values
 
 # middleware session config
 SESSION_OPTIONS = {
@@ -9,14 +8,14 @@ SESSION_OPTIONS = {
 
 # instagram api client configuration
 INSTAGRAM = {
-    'client_id': '',
-    'client_secret': '',
-    'redirect_uri': ''
+    'client_id': '6175f60283394342b0a720fb596ae0b6',
+    'client_secret': '67534712185146dc8ca6bbe94aabff7b',
+    'redirect_uri': 'http://localhost:8515/app'
 }
 
 # foursquare api client config
-FOURSQURE_CLIENT_ID = ''
-FOURSQURE_CLIENT_SECRET = ''
+FOURSQURE_CLIENT_ID = 'CA1FI3A2KJ3ZDRIUF5DJUZIXXES24XFVICYON34GBBOKBXSB'
+FOURSQURE_CLIENT_SECRET = 'FRHDILCEV0Y0E1WR3VWPWT03X0N01AKZU5SJVHK4M0NE2HF0'
 
 # foursquare categories config
 CATEGORY_NIGHTLIFE = 'nightlife'
