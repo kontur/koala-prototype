8bafcf856b0cf65826fbac71eee956b84516ad25
Johannes Neumeier <hello@johannesneumeier.com>
2015-01-28 21:35:26 +0200

    more API tweaking so we can pass in access_token from outside

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import bottle
-import beaker.middleware #TODO remove sessions everywhere
+import beaker.middleware  # TODO remove sessions everywhere
 import json
 import foursquare
 from bottle import route, redirect, post, run, request, hook, template, static_file, default_app
-from instagram import client
+from instagram import client, models
 import config
 import calendar
 import time
@@ -33,11 +33,11 @@ def join_all_categories():
     return str
 
 
-#########
+# ########
 # HELPERS
-#########
+# ########
 # def get_page():
-#     return template('index')
+# return template('index')
 
 
 def venues_search_place(place, category):
@@ -59,7 +59,7 @@ def venues_search_place(place, category):
 
 def venues_search_geolocation(lat, lng, category=None):
     fs = foursquare.Foursquare(client_id=config.FOURSQURE_CLIENT_ID, client_secret=config.FOURSQURE_CLIENT_SECRET)
-    params = {'ll': lat + ',' + lng,}
+    params = {'ll': lat + ',' + lng, }
     if category:
         if category in CATEGORIES.keys():
             params['categoryId'] = ''.join(CATEGORIES[category])
@@ -69,7 +69,7 @@ def venues_search_geolocation(lat, lng, category=None):
         params['categoryId'] = join_all_categories()
 
     results = fs.venues.explore(params=params)
-    #print results['groups']
+    # print results['groups']
 
     #TODO get this rate call working
     # print "RATE REMAINING", fs.rate_remaining()
@@ -79,31 +79,42 @@ def venues_search_geolocation(lat, lng, category=None):
     # return results
 
 
-def venues_images(foursquare_venue_id, sort_by=None):
-    access_token = request.session['access_token']
+def venues_images(access_token, foursquare_venue_id, sort_by=None):
     collection = []
-    if not access_token:
-        return 'Missing Access Token'
-    try:
-        api = client.InstagramAPI(access_token=access_token)
-        location = api.location_search(foursquare_v2_id=foursquare_venue_id)
+    # try:
+    api = client.InstagramAPI(access_token=access_token)
+    location = api.location_search(foursquare_v2_id=foursquare_venue_id)
+    # print "location", location
+    if len(location) > 0:
         media = api.location_recent_media(location_id=location[0].id)
+    # print "media", media
+    else:
+        return
 
+    try:
         for lst in media:
-            for medium in lst:
-                dict = {
-                    'type': medium.type,
-                    'image': medium.get_standard_resolution_url(),
-                    'likes': medium.like_count,
-                    'comments': medium.comment_count,
-                    'api_calls_remaining': api.x_ratelimit_remaining,
-                    'user': medium.user.username,
-                    'user_profile': medium.user.profile_picture,
-                    'created_time': time.asctime(medium.created_time.timetuple())
-                }
-                collection.append(dict)
+            if lst:
+                for medium in lst:
+                    # TODO WTF is going on here, why would there by some strings spread over an array in this lst?
+                    # print type(medium)
+                    if (medium and type(medium) is models.Media):
+
+                        # print "medium", medium
+                        dict = {
+                            'type': medium.type,
+                            'image': medium.get_standard_resolution_url(),
+                            'likes': medium.like_count,
+                            'comments': medium.comment_count,
+                            'api_calls_remaining': api.x_ratelimit_remaining,
+                            'user': medium.user.username,
+                            'user_profile': medium.user.profile_picture,
+                            'created_time': time.asctime(medium.created_time.timetuple())
+                        }
+                        collection.append(dict)
     except Exception as e:
         print(e)
+        # return e
+
 
     if sort_by and sort_by is "popular":
         collection = sorted(collection, key=lambda k: k['likes'])
@@ -111,7 +122,8 @@ def venues_images(foursquare_venue_id, sort_by=None):
 
     return collection
 
-############
+
+# ###########
 # API ROUTES
 ############
 
@@ -119,10 +131,23 @@ def venues_images(foursquare_venue_id, sort_by=None):
 @route('/api/venues/search/<term>/<category>')
 def find_venues(term, category=None):
     print "term:", term, "category:", category
+
+    if 'access_token' in request.query.keys():
+        access_token = request.query['access_token']
+        if not access_token:
+            return 'Invalid access_token'
+    else:
+        return 'Missing access_token'
+
+    if 'limit' in request.query.keys():
+        limit = request.query['limit']
+    else:
+        limit = 3
+
     venues = venues_search_place(term, category)
     venues_in_category = []
     i = f = 0
-    max = min(3, len(venues) - 1)
+    max = min(limit, len(venues) - 1)
     if venues:
         while (f < max):
             collection = venues_images(venues[i]['venue']['id'], "popular")
@@ -140,72 +165,49 @@ def find_venues(term, category=None):
 def get_venues(lat, lng, category=None):
     print "lat:", lat, "lng:", lng, "category:", category
 
-    # TODO smarter way of pushing possibly existing request.query access_token to session for all calls
     if 'access_token' in request.query.keys():
-        request.session['access_token'] = request.query['access_token']
+        access_token = request.query['access_token']
+        if not access_token:
+            return 'Invalid access_token'
+    else:
+        return 'Missing access_token'
 
+    if 'limit' in request.query.keys():
+        limit = request.query['limit']
+    else:
+        limit = 3
 
     venues = venues_search_geolocation(lat, lng, category)
     venues_in_category = []
     i = f = 0
-    max = min(3, len(venues) - 1)
+    max = min(limit, len(venues) - 1)
     if venues:
         while (f < max):
-            collection = venues_images(venues[i]['venue']['id'], "popular")
-            if len(collection) > 0:
+            collection = venues_images(access_token, venues[i]['venue']['id'], "popular")
+            if collection and len(collection) > 0:
                 f = f + 1
                 venues[i]['instagram'] = collection[0]
                 venues[i]['instagram_stats'] = {'num_photos': len(collection)}
             venues_in_category.append(venues[i])
             i = i + 1
+
     return json.dumps(venues_in_category)
 
 
 @route('/api/venue/<id>')
 def location_media(id):
+
+    if 'access_token' in request.query.keys():
+        access_token = request.query['access_token']
+        if not access_token:
+            return 'Invalid access_token'
+    else:
+        return 'Missing access_token'
+
     collection = venues_images(id, "popular")
     return json.dumps(collection)
 
-#
-# ############
-# # APP ROUTES
-# ############
-#
-# @route('/static/<filepath:path>')
-# def server_static(filepath):
-#     return static_file(filepath, root='./static')
-#
-#
-# @route('/')
-# def home():
-#     try:
-#         url = unauthenticated_api.get_authorize_url(scope=["likes", "comments"])
-#         return '<a href="%s">Connect with Instagram</a>' % url
-#     except Exception as e:
-#         print(e)
-#
+
 @route('/')
 def home():
     return '<a href="http://koala-prototype.meteor.com">prototype frontend moved, click me :)</a>'
-
-#
-# @route('/app')
-# def on_callback():
-#     code = request.GET.get("code")
-#     if not code:
-#         access_token = request.session['access_token']
-#         if not access_token:
-#             return 'Missing code'
-#     try:
-#         access_token, user_info = unauthenticated_api.exchange_code_for_access_token(code)
-#         if not access_token:
-#             return 'Could not get access token'
-#         api = client.InstagramAPI(access_token=access_token)
-#         request.session['access_token'] = access_token
-#         print ("access token=" + access_token)
-#     except Exception as e:
-#         print(e)
-#     return get_page()
-
-
-# bottle.run(app=app, host='localhost', port=5000, reloader=True)
